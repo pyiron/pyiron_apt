5681b4cc82cabf0b809f5d33879bc4695fb60c79
Sarath <sarathmenon@mailbox.org>
2024-10-02 12:42:08 +0200

    update visualise

--- nodes/visualize.py
+++ nodes/visualize.py
@@ -1,6 +1,8 @@
 from pyiron_workflow import Workflow
 
-@Workflow.wrap.as_function_node('visualize_hdf')
+
+@Workflow.wrap.as_function_node("visualize_hdf")
 def visualize_hdf(results_file_path):
     from jupyterlab_h5web import H5Web
+
     return H5Web(results_file_path)
